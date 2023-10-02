8a9357b7251ff499f200fc20d24955c397d17552
rahat.adnan <sthep90@Gmail.com>
2023-10-02 12:56:57 +0600

    Time Range Function Under Implementation

--- Time.js
+++ Time.js
@@ -1,4 +1,54 @@
 'use strict';
+//under implementation
+class TimeRange{
+  constructor(date1,date2) {
+    if(isNaN(date1.getTime()) || isNaN(date2.getTime())) throw new Error(`Invalid Date. Failed to create Range`);
+      this.startDate = date1.getTime()<=date2.getTime()?date1:date2;
+      this.endDate = date2.getTime()<=date1.getTime()?date1:date2;
+  }
+  isSameDate(){
+    return this.startDate.getDate() === this.endDate.getDate() && this.startDate.getMonth() === this.endDate.getMonth() && this.startDate.getFullYear() === this.endDate.getFullYear();
+  }
+  isSameDay(){
+
+  }
+  isSameHour(){
+
+  }
+  isSameMinute(){
+
+  }
+  isSameSecond(){
+
+  }
+  isSameMilliSecond(){
+
+  }
+  isSameMonth(){
+
+  }
+  isSameYear(){
+
+  }
+  isSameWeek(){
+
+  }
+
+  difference(){
+    const diff = {};
+    diff.years = Math.abs(this.endDate.getFullYear() - this.startDate.getFullYear());
+    diff.months = Math.abs(this.endDate.getMonth() - this.startDate.getMonth());
+    diff.days = Math.abs(this.endDate.getDate() - this.startDate.getDate());
+    diff.hours = Math.abs(this.endDate.getHours() - this.startDate.getHours());
+    diff.minutes = Math.abs(this.endDate.getMinutes() - this.startDate.getMinutes());
+    diff.seconds = Math.abs(this.endDate.getSeconds() - this.startDate.getSeconds());
+    return diff;
+  }
+}
+class Clock{
+
+}
+
 module.exports = class Time {
   #__MONTHS__=[{name:"January",short:"jan",index:0},{name:"February",short:"feb",index:1},{name:"March",short:"mar",index:2},{name:"April",short:"apr",index:3},{name:"May",short:"may",index:4},{name:"June",short:"jun",index:5},{name:"July",short:"jul",index:6},{name:"August",short:"aug",index:7},{name:"September",short:"sep",index:8},{name:"October",short:"oct",index:9},{name:"November",short:"nov",index:10},{name:"December",short:"dec",index:11}]
   constructor(time=new Date()) {
@@ -7,7 +57,7 @@ module.exports = class Time {
     this.Year = this.date.getFullYear();
     this.Month = this.#__MONTHS__.find(item=>item.index===this.date.getMonth()).short;
   }
-  __processTime(conf){
+  #__processTime(conf){
     const __DAYS__={0:{name:"Sunday",short:"Sun",date_index:0},1:{name:"Monday",short:"Mon",date_index:1},2:{name:"Tuesday",short:"Tues",date_index:2},3:{name:"Wednesday",short:"Wed",date_index:3},4:{name:"Thursday",short:"Thur",date_index:4},5:{name:"Friday",short:"Fri",date_index:0},6:{name:"Saturday",short:"Sat",date_index:6}};
     const getLastDayOfMonth=(t,n)=>new Date(n,t+1,0).toString().split(" ")[2];
     //conf parsing
@@ -100,13 +150,13 @@ module.exports = class Time {
     throw new Error('Invalid Month Error')
   }
   setYear(year){
-    if(isNaN(year) || Number(year)<1000) throw new Error('Incompatible year.');
+    if(isNaN(year) || Number(year)<1970) throw new Error('Incompatible year.');
     this.date.setFullYear(year);
     this.Month = this.#__MONTHS__.find(item=>item.index===this.date.getMonth()).short;
     this.Year = this.date.getFullYear();
   }
   monthInfo(shortDayName=true){
-    return this.__processTime({
+    return this.#__processTime({
       year: this.Year,
       month:this.Month,
       complete_year:false,
@@ -114,12 +164,19 @@ module.exports = class Time {
     })
   }
   yearlyInfo(shortDayName=true){
-    return this.__processTime({
+    return this.#__processTime({
       year: this.Year,
       month:this.Month,
       complete_year:true,
       day_name_short: Boolean(shortDayName)
     })
+  }
+
+  static createRange(start_time,end_time){
+    return new TimeRange(new Date(start_time),new Date(end_time));
+  }
+  static clock(){
+
   }
 
 }
